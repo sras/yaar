f638c7db97617cbe366b2d3634bb7aebc6c6e911
Sandeep.C.R <sandeepcr2@gmail.com>
2018-08-22 18:36:46 +0530

    Routing fixes

--- src/Yaar/Routing.hs
+++ src/Yaar/Routing.hs
@@ -1,52 +1,64 @@
 {-# Language BangPatterns #-}
 {-# Language OverloadedStrings #-}
+{-# Language DeriveGeneric #-}
+{-# Language DeriveAnyClass #-}
 
 module Yaar.Routing (Routes, lookupRequest, makeRoutes) where
 
-import Data.Text (pack, unpack, Text)
+import Data.Text as T (concat, intercalate, pack, unpack, Text)
 import Data.Text.Encoding (decodeUtf8)
-import Data.Map.Strict as DM (Map, empty, lookup, insert)
+import Data.Map.Strict as DM (Map, assocs, empty, lookup, insert)
 import Network.Wai ( Request , requestMethod , pathInfo)
+import Data.Maybe (listToMaybe)
+import Control.DeepSeq (NFData)
+import Control.Exception
+import GHC.Generics
 
-data RouteSegment = MethodSegment Text | ParamSegment | UrlSegment Text deriving (Eq, Ord, Show) 
+data RouteSegment = MethodSegment Text | ParamSegment | UrlSegment Text deriving (Eq, Ord, Generic, NFData)
 
-data Routes = RouteTree (Map RouteSegment Routes) | RouteIndex Int deriving (Show)
+data SegmentsWithIndex = SegmentsWithIndex Int [RouteSegment]
 
-toRouteSegments :: [Text] -> [RouteSegment]
-toRouteSegments (x:xs) = (MethodSegment x): (toUrlSegment <$> xs)
+data Routes = RouteTree Int (Map RouteSegment Routes) deriving (Show, NFData, Generic)
+-- Route with an Precedence field
+
+instance Show RouteSegment where
+  show (UrlSegment x) = unpack x
+  show ParamSegment = "::param::"
+  show (MethodSegment x) = unpack x
+
+toRouteSegments :: [Text] -> Int -> SegmentsWithIndex
+toRouteSegments (x:xs) p = SegmentsWithIndex p $ (MethodSegment x): (toUrlSegment <$> xs)
   where
     toUrlSegment :: Text -> RouteSegment
     toUrlSegment "::param::" = ParamSegment
     toUrlSegment x = UrlSegment x
 
 makeRoutes :: [[Text]] -> Routes
-makeRoutes !x = foldl insertRoute (RouteTree $ DM.empty) $ zip (toRouteSegments . putMethodInfront <$> x) [0..]
+makeRoutes !x = foldl insertRoute (RouteTree 0 $ DM.empty) $ (zipWith toRouteSegments (putMethodInfront <$> x) [0..])
   where
   putMethodInfront :: [Text] -> [Text]
   putMethodInfront x = (last x):(init x)
-  insertRoute :: Routes -> ([RouteSegment], Int) -> Routes
-  insertRoute (RouteTree map)  ([s], idx) = case lookupSegment s map of
-    Nothing -> RouteTree $ DM.insert s (RouteIndex idx) map
-    Just _ -> error "Overlapping routes found!"
-  insertRoute (RouteTree map) ((r:rs), idx) = case lookupSegment r map of
-    Just m -> RouteTree $ DM.insert r (insertRoute m (rs, idx)) map
-    Nothing -> RouteTree $ DM.insert r (insertRoute (RouteTree DM.empty) (rs, idx)) map
-  lookupSegment :: RouteSegment -> Map RouteSegment Routes -> Maybe Routes
-  lookupSegment r m = case DM.lookup r m of
-    Just a -> Just a
-    Nothing -> DM.lookup ParamSegment m
+  insertRoute ::Routes -> SegmentsWithIndex -> Routes
+  insertRoute (RouteTree _ map) (SegmentsWithIndex p []) = emptyTree p
+  insertRoute (RouteTree o map) (SegmentsWithIndex p (r:rs)) = case DM.lookup r map of
+    Nothing -> RouteTree o $ DM.insert r (insertRoute (emptyTree p) (SegmentsWithIndex p rs)) map
+    Just submap -> RouteTree o $ DM.insert r (insertRoute submap (SegmentsWithIndex p rs)) map
+  emptyTree p = RouteTree p DM.empty
 
 lookupRequest :: Request -> Routes -> Maybe Int
 lookupRequest r !routes = let
-  routeSegments = toRouteSegments $ (decodeUtf8 $ requestMethod r):(pathInfo r)
-  in lookUp routes routeSegments
-  where
-    lookUp :: Routes -> [RouteSegment] -> Maybe Int
-    lookUp (RouteIndex i) [] = Just i
-    lookUp routes@(RouteTree map) [] = Nothing
-    lookUp routes@(RouteTree map) (r:rs) = case DM.lookup r map of
-        Just x -> lookUp x rs
-        Nothing -> case DM.lookup ParamSegment map of
-          Just x -> lookUp x rs
-          Nothing -> Nothing
+  SegmentsWithIndex _ routeSegments = toRouteSegments ((decodeUtf8 $ requestMethod r):(pathInfo r)) 0
+  in lookupRoute routes routeSegments
 
+lookupRoute :: Routes -> [RouteSegment] -> Maybe Int
+lookupRoute routes@(RouteTree p _) [] = Just p
+lookupRoute routes@(RouteTree _ map) (r:rs) =
+  -- Look for an exact match for the segment 'r' and for a wild card match
+  -- if both are found use the one with a higher precedence
+  -- or else just use the available one.
+  -- It none of them are found return Nothing indicating a route lookup failure
+  case (DM.lookup ParamSegment map, DM.lookup r map) of
+    (Just x@(RouteTree px _), Just w@(RouteTree pw _)) -> if px < pw then lookupRoute x rs else lookupRoute w rs
+    (Just x, Nothing) -> lookupRoute x rs
+    (Nothing, Just x) -> lookupRoute x rs
+    (Nothing, Nothing) -> Nothing
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,101 @@
+{-# Language OverloadedStrings #-}
+
+import Yaar.Routing
+import Test.Hspec
+import Network.Wai.Internal
+import Network.Wai (defaultRequest)
+import Control.Exception
+import Control.DeepSeq (force)
+import Data.Text (Text)
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+  describe "routing" $ do
+    it "should match when there is a param route but no exact match" $
+      let
+        request = defaultRequest { requestMethod = "GET", pathInfo = ["seg1", "seg2", "seg3"] }
+        routes = makeRoutes
+          [ [ "seg1"
+          , "seg4"
+          , "seg3"
+          , "GET" ]
+          , [ "seg1"
+          , "::param::"
+          , "seg3"
+          , "GET"
+          ]
+          ]
+      in lookupRequest request routes `shouldBe` (Just 1)
+    it "should return param route match even when there is an exact match of lower precedence" $
+      let
+        request = defaultRequest { requestMethod = "GET", pathInfo = ["seg1", "seg4", "seg3"] }
+        routes = makeRoutes
+          [ [ "seg1"
+          , "seg4"
+          , "::param::"
+          , "GET"
+          ]
+          , [ "seg1"
+          , "seg4"
+          , "seg3"
+          , "GET"
+          ]
+          ]
+      in lookupRequest request routes `shouldBe` (Just 0)
+    it "should return exact route match even when there a param route of lower precedence" $
+      let
+        request = defaultRequest { requestMethod = "GET", pathInfo = ["seg1", "seg4", "seg3"] }
+        routes = makeRoutes
+          [ [ "seg1"
+          , "seg4"
+          , "seg3"
+          , "GET" ]
+          , [ "seg1"
+          , "::param::"
+          , "seg3"
+          , "GET"
+          ]
+          ]
+      in lookupRequest request routes `shouldBe` (Just 0)
+    it "should match when incoming route exactly match with one in routes" $
+      let
+        request = defaultRequest { requestMethod = "GET", pathInfo = ["seg1", "seg4", "seg3"] }
+        routes = makeRoutes
+          [ [ "seg1"
+            , "seg4"
+            , "seg3"
+            , "GET"
+            ]
+          ]
+      in lookupRequest request routes `shouldBe` Just 0
+    it "should return Nothing when there is no matching route" $
+      let
+        request = defaultRequest { requestMethod = "GET", pathInfo = ["seg1", "seg2", "seg2"] }
+        routes = makeRoutes
+          [ [ "seg1"
+          , "seg4"
+          , "seg3"
+          , "GET" ]
+          , [ "seg1"
+          , "::param::"
+          , "seg3"
+          , "GET"
+          ]
+          ]
+      in lookupRequest request routes `shouldBe` Nothing
+    it "should match correct one when there are many routes defined" $
+      let
+        request = defaultRequest { requestMethod = "GET", pathInfo = ["seg5", "seg6", "seg7"] }
+        routes = makeRoutes
+          [ [ "seg1"
+            , "seg4"
+            , "seg3"
+            , "GET"
+            ]
+          , [ "seg5"
+            , "seg6"
+            , "seg7"
+            , "GET"
+            ]
+          ]
+      in lookupRequest request routes `shouldBe` Just 1
